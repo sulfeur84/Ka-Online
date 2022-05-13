d3d58a10cbe054a1ce870c0229265b39a66c4151
sulfeur84 <sulfeur84@gmail.com>
2022-05-13 14:24:24 +0200

    Menu

---
+++ Assets/Scripts/SpriteAlea.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class SpriteAlea : MonoBehaviour
+{
+    public List<Sprite> sprite = new List<Sprite>();
+
+    public Image img;
+    private void Start()
+    {
+        int alea = Random.Range(0, sprite.Count);
+
+        img.sprite = sprite[alea];
+    }
+}
